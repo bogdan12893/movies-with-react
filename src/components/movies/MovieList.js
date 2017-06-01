5772234297c03bb4fc0c3933355b133f0e3444fc
Bogdan Cadar <bogdan1281993@gmail.com>
2017-06-01 09:41:31 +0300

    MovieList set on state + clean render

--- src/components/movies/MovieList.js
+++ src/components/movies/MovieList.js
@@ -4,33 +4,38 @@ import Search from './Search';
 import './MovieList.css';
 
 export default class MovieList extends React.Component {
-  constructor(props) {
+    constructor(props) {
     super(props);
 
     this.state = {
-      searchItem: ''
+      searchItem: '',
+      movies: props.movies
     };
     
     this.updateSearch = this.updateSearch.bind(this);
   }
-
+  
   updateSearch(event) {
     this.setState({ searchItem: event.target.value });
   }
 
+  filterMovieListBySearchItem = () => {
+    return this.state.movies.filter(movie => {
+      return movie.name.toLowerCase().indexOf(this.state.searchItem.toLocaleLowerCase()) !== -1;
+    });
+  }
+
+  renderMovieList = () => {
+    let filteredMovies = this.filterMovieListBySearchItem();
+    return filteredMovies.map(movie => <Movie movie={movie} key={movie.id} /> );
+  }
+
   render() {
-    let filteredMovies = this.props.movies
-      .filter(movie => {
-        return movie.name.toLowerCase().indexOf(this.state.searchItem) !== -1;
-      });
-    
     return (
       <div>
         <ul>
           <Search onSearch={this.updateSearch} />
-          {filteredMovies.map((movie) => {
-            return <Movie movie={movie} key={movie.id} />
-          })}
+          { this.renderMovieList() }
         </ul>
       </div>
     );
